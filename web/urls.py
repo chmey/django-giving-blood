47f8b752d4f7385aa535a7f4aab80a77603b044c
Klaus Christoph Meyer <klaus.meyer@rub.de>
2019-05-07 19:04:28 +0200

    Standard Django URLS for pw reset flow

--- web/urls.py
+++ web/urls.py
@@ -5,12 +5,12 @@ from django.contrib.auth import views as auth_views
 # app_name = 'web'
 urlpatterns = [
         path('', views.index, name='index'),
-        path('login', auth_views.LoginView.as_view(template_name='web/login.html'), name='login'),
-        path('logout', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
-        path('signup', views.signup, name='signup'),
+        path('auth/login', auth_views.LoginView.as_view(template_name='web/login.html'), name='login'),
+        path('auth/logout', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+        path('auth/signup', views.signup, name='signup'),
         path('profile', views.profile, name='profile'),
-        path('auth/reset/init', auth_views.PasswordResetView.as_view(), name="reset-pw"),
-        path('auth/reset/done', auth_views.PasswordResetDoneView.as_view(), name="reset-pw-done"),
-        path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='reset-pw-confirm'),
-        path('auth/reset/done', auth_views.PasswordResetCompleteView.as_view(), name='reset-pw-complete'),
+        path('auth/reset/init', auth_views.PasswordResetView.as_view(), name="assword_reset"),
+        path('auth/reset/done', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+        path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+        path('auth/reset/done', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     ]
