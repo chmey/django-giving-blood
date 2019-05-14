ff5307010c7ea0a5c4bba767d6410abbfb8ccb52
Julian Knorr <git@jknorr.eu>
2019-05-14 20:26:36 +0200

    Implement rhesus factor and set the values according to octal number system.

--- web/models.py
+++ web/models.py
@@ -10,11 +10,16 @@ class Profile(models.Model):
     GENDER_DIVERSE = 2
     GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female'), (GENDER_DIVERSE, 'Diverse')]
 
-    BLOODTYPE_0 = 0
-    BLOODTYPE_A = 1
-    BLOODTYPE_B = 2
-    BLOODTYPE_AB = 3
-    BLOODTYPE_CHOICES = [(BLOODTYPE_0, '0'), (BLOODTYPE_A, 'A'), (BLOODTYPE_B, 'B'), (BLOODTYPE_AB, 'AB')]
+    BLOODTYPE_0n = 0
+    BLOODTYPE_0p = 1
+    BLOODTYPE_An = 2
+    BLOODTYPE_Ap = 3
+    BLOODTYPE_Bn = 4
+    BLOODTYPE_Bp = 5
+    BLOODTYPE_ABn = 6
+    BLOODTYPE_ABp = 7
+    BLOODTYPE_CHOICES = [(BLOODTYPE_0n, '0-'), (BLOODTYPE_0p, '0+'), (BLOODTYPE_An, 'A-'), (BLOODTYPE_Ap, 'A+'),
+                         (BLOODTYPE_Bn, 'B-'), (BLOODTYPE_Bp, 'B+'), (BLOODTYPE_ABn, 'AB-'), (BLOODTYPE_ABp, 'AB+')]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.SmallIntegerField(null=True, blank=True, choices=GENDER_CHOICES)
