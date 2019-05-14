84d45c4bf576555ec8654b3a3969021b937cbc7c
Klaus Christoph Meyer <klaus.meyer@rub.de>
2019-05-14 22:36:40 +0200

    Password reset and change flow
    	new file:   web/templates/auth/password_change.html
    	new file:   web/templates/auth/password_change_done.html
    	new file:   web/templates/auth/password_reset_complete.html
    	new file:   web/templates/auth/password_reset_confirm.html
    	new file:   web/templates/auth/password_reset_done.html
    	new file:   web/templates/auth/password_reset_email.html
    	new file:   web/templates/auth/password_reset_init.html
    	new file:   web/templates/auth/password_reset_subject.txt

--- web/urls.py
+++ web/urls.py
@@ -5,13 +5,19 @@ from django.contrib.auth import views as auth_views
 # app_name = 'web'
 urlpatterns = [
         path('', views.index, name='index'),
-        path('auth/login', auth_views.LoginView.as_view(template_name='web/login.html',redirect_authenticated_user=True), name='login'),
+        path('auth/login', auth_views.LoginView.as_view(template_name='auth/login.html',redirect_authenticated_user=True), name='login'),
         path('auth/logout', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
         path('auth/signup', views.signup, name='signup'),
         path('profile', views.profile, name='profile'),
         path('profile/edit', views.edit_profile, name='edit_profile'),
-        path('auth/reset/init', auth_views.PasswordResetView.as_view(), name="assword_reset"),
-        path('auth/reset/done', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-        path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-        path('auth/reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+        path('auth/change_password', auth_views.PasswordChangeView.as_view(template_name='auth/password_change.html'), name='password_change'),
+        path('auth/change_password/done', auth_views.PasswordChangeDoneView.as_view(template_name='auth/password_change_done.html'), name='password_change_done'),
+        path('auth/reset/init', auth_views.PasswordResetView.as_view(
+                template_name='auth/password_reset_init.html',
+                email_template_name='auth/password_reset_email.html',
+                subject_template_name='auth/password_reset_subject.txt'), name='password_reset'),
+        path('auth/reset/done', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+        path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
+        path('auth/reset/complete', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
+
     ]
