a5d784d1e8d28fad15e3f0174d44bd698badc09f
Klaus Christoph Meyer <klaus.meyer@rub.de>
2019-05-22 12:37:30 +0200

    Custom Admin Site

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
+from .models import DonationPlace, Donation, Profile
+from django.contrib.auth.models import User
 
-# Register your models here.
+class BloodAdminSite(AdminSite):
+    site_header = 'Administration'
+
+admin_site = BloodAdminSite(name='bloodadmin')
+admin_site.register([Donation, DonationPlace, Profile])
