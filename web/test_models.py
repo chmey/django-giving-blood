5e89a9a864507b329af88583c2d5c204c8aed802
Julian Knorr <git@jknorr.eu>
2019-06-11 13:47:45 +0200

    Add Donation Place Model Test Cases

--- web/test_models.py
+++ web/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Donation
+from .models import Donation, DonationPlace
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 
@@ -26,3 +26,27 @@ class ProfileModelTest(TestCase):
 
     def testGetNextDonationDate(self):
         pass
+
+
+class DonationPlaceModelTest(TestCase):
+
+    def testGetAddress(self):
+        u = User.objects.create()
+        d = DonationPlace.objects.create(name="Place Name", street="Ul. Przykład", house="3a",
+                                         address_supplement="M. 4c", postal_code="10-234", city="Kraków", country="PL",
+                                         contributor=u)
+        self.assertSequenceEqual(d.get_address(), "Ul. Przykład 3a, M. 4c, 10-234 Kraków, POLAND")
+
+    def testGetStr(self):
+        u = User.objects.create()
+        d = DonationPlace.objects.create(name="Place Name", street="Ul. Przykład", house="3a",
+                                         address_supplement="M. 4c", postal_code="10-234", city="Kraków", country="PL",
+                                         contributor=u)
+        self.assertSequenceEqual(str(d), "Place Name, Ul. Przykład 3a, M. 4c, 10-234 Kraków, POLAND")
+
+    def testGetUrl(self):
+        u = User.objects.create()
+        d = DonationPlace.objects.create(name="Place Name", street="Ul. Przykład", house="3a",
+                                         address_supplement="M. 4c", postal_code="10-234", city="Kraków", country="PL",
+                                         contributor=u)
+        self.assertSequenceEqual(d.get_maps_url(), "https://www.google.com/maps/?q=Ul.%20Przyk%C5%82ad%203a,%20M.%204c,%2010-234%20Krak%C3%B3w,%20POLAND")
